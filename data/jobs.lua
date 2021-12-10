2ee715adb9c7adb8005b2a158d8c291914de5f4d
ThatGuyBill <95900744+ThatGuyBill@users.noreply.github.com>
2021-12-10 15:46:57 +1000

    jobs

---
+++ data/jobs.lua
@@ -0,0 +1,29 @@
+local Jobs = {
+    ['unemployed'] = {
+        name = "Unemployed",
+        grade = {
+            [1] = {label = "Unemployed", salary = 200},
+        },
+    },
+
+    ['police'] = {
+        name = "Police Officer",
+        grade = {
+            [1] = {label = "Cadet", salary = 200},
+            [2] = {label = "Trooper", salary = 200},
+            [3] = {label = "Corporal", salary = 200},
+            [4] = {label = "Sergeant", salary = 200},
+            [5] = {label = "Staff Sergeant", salary = 200},
+            [6] = {label = "Inspector", salary = 200},
+            [7] = {label = "Lieutenant", salary = 200},
+            [8] = {label = "Captain", salary = 200},
+            [9] = {label = "Major", salary = 200},
+            [10] = {label = "Commander", salary = 200},
+            [11] = {label = "Lieutenant Colonel", salary = 200},
+            [12] = {label = "Assistant Chief", salary = 200},
+            [13] = {label = "Chief of Police", salary = 200},
+        },
+    },
+
+}
+return Jobs
